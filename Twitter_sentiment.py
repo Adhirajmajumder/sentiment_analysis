c7fd0f531ec97644c49480747dfe3606b5999a7c
unknown <adhirajmajumder@gmail.com>
2020-03-17 00:32:34 +0530

    Twitter sentiment analysis code set

--- Twitter_sentiment.py
+++ Twitter_sentiment.py
@@ -4,8 +4,7 @@ Created on Wed Mar 11 23:16:30 2020
 
 @author: ADHIRAJ MAJUMDAR
 """
-
-#import re 
+ 
 import tweepy 
 from tweepy import OAuthHandler 
 from textblob import TextBlob
@@ -35,20 +34,12 @@ class TwitterClient(object):
         except: 
             print("Error: Authentication Failed") 
   
-#    def clean_tweet(self, tweet): 
-#        ''' 
-#        Utility function to clean tweet text by removing links, special characters 
-#        using simple regex statements. 
-#        '''
-#        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split()) 
-  
+
     def get_tweet_sentiment(self, tweet): 
         ''' 
         Utility function to classify sentiment of passed tweet 
         using textblob's sentiment method 
         '''
-        # create TextBlob object of passed tweet text 
-#        analysis = TextBlob(self.clean_tweet(tweet))
         analysis = TextBlob(tweet)
         # set sentiment 
         if analysis.sentiment.polarity > 0: 
